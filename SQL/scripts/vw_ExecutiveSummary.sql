cc73d1119937c10219329765f709bc685ea72f9a
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2026-06-10 15:30:43 +0100

    view, index and csv uploads

---
+++ SQL/scripts/vw_ExecutiveSummary.sql
@@ -0,0 +1,7 @@
+CREATE VIEW vw_ExecutiveSummary AS
+SELECT
+SUM(SalesAmount) AS TotalRevenue,
+SUM(TotalProductCost) AS TotalCost,
+SUM(Profit) AS TotalProfit,
+AVG(MarginPct) AS AvgMargin
+FROM dbo.vw_FactFinancials;
